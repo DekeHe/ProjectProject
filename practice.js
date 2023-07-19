e37697f721c3e67b036ba403bf5ba672813cb56b
DekeHe <98167199+DekeHe@users.noreply.github.com>
2023-07-19 12:40:04 -0400

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,4 +1,16 @@
+class
 Vegetable=function(size,color){this.size=size;this.color=color}
 Vegetable.prototype.run=function(){console.log('run')}
 
-Cabbage=function(size,color,shape){Vegetable.call(this,size)}
+inheritance
+Cabbage=function(size,color,shape){Vegetable.call(this,size,color);this.shape=shape}
+Cabbage.prototype.run=function(){console.log('run run')}
+
+v=new Vegetable(1,'red')
+console.log(v,v.size,v.color)
+v.run()
+
+c=new Cabbage(1,'red','square')
+console.log(c,c.size,c.color,c.shape)
+c.run()
+
