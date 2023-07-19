b7c87a94957ca99d99ba890eb25d05b13b4691a8
DekeHe <98167199+DekeHe@users.noreply.github.com>
2023-07-19 14:46:24 -0400

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,9 +1,11 @@
+js
 class
 Vegetable=function(size,color){this.size=size;this.color=color}
 Vegetable.prototype.run=function(){console.log('run')}
 
 inheritance
 Cabbage=function(size,color,shape){Vegetable.call(this,size,color);this.shape=shape}
+Cabbage.prototype=Object.create(Vegetable.prototype)
 Cabbage.prototype.run=function(){console.log('run run')}
 
 v=new Vegetable(1,'red')
@@ -14,3 +16,8 @@ c=new Cabbage(1,'red','square')
 console.log(c,c.size,c.color,c.shape)
 c.run()
 
+firstofall
+test project
+
+mongodb
+https://stackoverflow.com/questions/65357744/how-to-install-mongodb-on-apple-m1-chip
